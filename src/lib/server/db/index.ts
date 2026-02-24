dd250d47ff75447f0e0ae01bb8137f8407e937a3
Daley Chen <mail@daleychen.com>
2026-02-23 21:50:48 -0500

    dynamic env is not available on build time

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,10 +1,9 @@
 import { drizzle } from 'drizzle-orm/neon-http';
 import * as schema from './schema';
-import { env } from '$env/dynamic/private';
 import { neon } from '@neondatabase/serverless';
 
 export function initDatabase(url?: string) {
-	const client = neon(url ?? env.DATABASE_URL);
+	const client = neon(url ?? process.env.DATABASE_URL);
 
 	return drizzle(client, { schema });
 }
